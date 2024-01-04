d222a80cae5e3ab07ad819221f9ca02c308caaac
henbarper <benjaminitonio@gmail.com>
2024-01-04 14:13:22 -0600

    2 v3

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -13,8 +13,8 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 def measure_time(n: int, max_delay: float) -> float:
     """Use the time module to measure
     an approximate elapsed time."""
-    start_time = time.time()
+    start_time: float = time.time()
     asyncio.run(wait_n(n, max_delay))
-    end_time = time.time()
-    total_time = end_time - start_time
+    end_time: float = time.time()
+    total_time: float = end_time - start_time
     return total_time / n

934461b31a415974a7a68abb08c87f4fc7483218
henbarper <benjaminitonio@gmail.com>
2024-01-04 15:05:12 -0600

    0 v2

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 """Write a coroutine called async_generator that takes no arguments."""
 import random
 import asyncio
+import typing
 
 
-async def async_generator():
+async def async_generator() -> typing.Generator:
     """The coroutine will loop 10 times, each time
     asynchronouslywait 1 second, then yield a random
     number between 0 and 10. Use the random module."""

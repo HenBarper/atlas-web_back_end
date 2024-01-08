d306c3e2d075c2168a0047aaa4aca7f1244df580
henbarper <benjaminitonio@gmail.com>
2024-01-08 13:56:16 -0600

    1 v1

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-"""Create a class BasicCache that
-inherits from BaseCaching and
-is a caching system:"""
+""""""
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class BasicCache(BaseCaching):
-    """Basic cache class"""
+class FIFOCache(BaseCaching):
+    """"""
+    def __init__(self):
+        """"""
+        super().__init__()
 
     def put(self, key, item):
         """Put function of basic cache"""
         if key is None or item is None:
             pass
+        elif len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            print(f"DISCARD: {self.cache_data[0].key}")
+            self.cache_data[0]
         else:
             self.cache_data[key] = item
 

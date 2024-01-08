711d0e0bde748e10e8e24772f7363f38a6e895e1
henbarper <benjaminitonio@gmail.com>
2024-01-08 13:32:54 -0600

    0 v2

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-""""""
+"""Create a class BasicCache that
+inherits from BaseCaching and
+is a caching system:"""
 BaseCaching = __import__('base_caching').BaseCaching
+
 
 class BasicCache(BaseCaching):
     """Basic cache class"""

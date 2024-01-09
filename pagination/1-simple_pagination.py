b57323905646a99bb676e8c2dd39edab4d196d19
henbarper <benjaminitonio@gmail.com>
2024-01-09 14:41:13 -0600

    1 v1

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""Implement a method named get_page
+that takes two integer arguments page
+with default value 1 and page_size with
+default value 10."""
+
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of
+    popular baby names."""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+            pass
+
+
+def index_range(page: int, page_size: int):
+    """The function should return a tuple
+    of size two containing a start index
+    and an end index corresponding to the
+    range of indexes to return in a list
+    for those particular pagination parameters."""
+    index_tuple = (((page * page_size) - page_size), page * page_size)
+    return index_tuple
+
+
+def get_page(page: int = 1, page_size: int = 10):
+    """"""
+    assert page is int, "page is not int"
+    assert page_size is int, "page_size is not int"
+    assert page > 0, "page is not greater than zero"
+    assert page_size > 0, "page_size is not greater than zero"
+
+    index_tuple = index_range(page, page_size)
+
+    with open('Popular_Baby_Names.csv', 'r') as csv_file:
+        # Create a CSV reader object
+        csv_reader = csv.reader(csv_file)
+
+        row_list = []
+
+        if len(list(csv_file)) < index_tuple[1]:
+            return row_list
+
+        # Iterate through each row in the CSV file
+        for row in csv_reader:
+            if row >= index_tuple[0] and row <= index_tuple[1]:
+                row_list.append(row)
+        return row_list

31a65e8ed20ddc64ec4ba9c6f66b1a745a77eeaf
henbarper <benjaminitonio@gmail.com>
2024-01-09 12:57:26 -0600

    4 v1

---
+++ caching/4-mru_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Create a class MRUCache that
+inherits from BaseCaching and
+is a caching system:"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """MRU cache class"""
+    def __init__(self):
+        """MRU Cache  init method"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Put function of basic cache"""
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discarded_key = next(iter(self.cache_data))
+                print(f"DISCARD: {discarded_key}")
+                del self.cache_data[discarded_key]
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Get function of basic cache"""
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None

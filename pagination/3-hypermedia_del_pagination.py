4c26ca46f7907cd5f4987eac4a6f38d96792ab30
henbarper <benjaminitonio@gmail.com>
2024-01-10 14:55:42 -0600

    3 v3

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -41,6 +41,9 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """Implementation of get_hyper_index method with two
+        integer arguments: index with a None default value
+        and page_size with default value of 10."""
         assert index is None or 0 <= index < len(self.__indexed_dataset)
 
         data_set = []

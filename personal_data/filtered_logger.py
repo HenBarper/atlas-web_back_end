f7bf61721d549b1cdf7e16d09711ca4ad72771e9
henbarper <benjaminitonio@gmail.com>
2024-01-15 17:56:33 -0600

    0 v2

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
-""""""
+"""Write a function called filter_datum
+that returns the log message obfuscated"""
 import re
+import typing
 
 
-def filter_datum(fields, redaction, message, separator):
-    """"""
+def filter_datum(
+        fields: typing.List[str],
+        redaction: str,
+        message: str,
+        separator: str
+        ) -> str:
+    """The function should use a regex to replace
+    occurrences of certain field values"""
     for field in fields:
         pattern = r'({})([^;]+)'.format(field + '=')
         message = re.sub(pattern, r"\1{}".format(redaction), message)

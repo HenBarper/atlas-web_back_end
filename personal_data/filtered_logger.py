3761d81926c2e0c8b69467bc6b498f1527dd0e80
henbarper <benjaminitonio@gmail.com>
2024-01-15 18:00:25 -0600

    0 v3

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,15 +5,11 @@ import re
 import typing
 
 
-def filter_datum(
-        fields: typing.List[str],
-        redaction: str,
-        message: str,
-        separator: str
-        ) -> str:
+def filter_datum(fields: typing.List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """The function should use a regex to replace
     occurrences of certain field values"""
     for field in fields:
-        pattern = r'({})([^;]+)'.format(field + '=')
+        pattern = r'({})([^{}]+)'.format(field + '=', separator)
         message = re.sub(pattern, r"\1{}".format(redaction), message)
     return message

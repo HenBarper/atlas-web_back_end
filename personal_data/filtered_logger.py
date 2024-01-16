c4da2c18f9ca56cce4d952923e853f5c1e05225e
henbarper <benjaminitonio@gmail.com>
2024-01-16 15:03:18 -0600

    1 v3

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -25,10 +25,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: typing.List[str]):
+        """Init method for redacting formatter"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """Format method for redacting formatter"""
         record.msg = filter_datum(self.fields, self.REDACTION,
                                   record.msg, self.SEPARATOR)
         return super(RedactingFormatter, self).format(record)

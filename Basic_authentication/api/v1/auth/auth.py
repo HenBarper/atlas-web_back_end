0df2c339f9218fc22f5cfe14ea2a1376f54cc928
henbarper <benjaminitonio@gmail.com>
2024-01-17 13:40:59 -0600

    3 v3

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -5,17 +5,17 @@ from typing import List, TypeVar
 
 
 class Auth():
-    """"""
+    """class to manage the API authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """"""
+        """returns false"""
         return False
 
 
     def authorization_header(self, request=None) -> str:
-        """"""
+        """returns none"""
         return None
 
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """"""
+        """returns none"""
         return None

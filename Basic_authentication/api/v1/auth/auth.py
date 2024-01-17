3db09fb2e9477e7f2c1ada13473fb3d6921def76
henbarper <benjaminitonio@gmail.com>
2024-01-17 14:25:46 -0600

    4 v3

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -12,8 +12,11 @@ class Auth:
         """returns false"""
         if path is None or excluded_paths is None or excluded_paths == []:
             return True
-        elif path in excluded_paths:
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
             return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """returns none"""

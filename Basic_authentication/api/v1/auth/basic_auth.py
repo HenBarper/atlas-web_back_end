054583517319e66400adaa7c1e9c6effd615e50e
henbarper <benjaminitonio@gmail.com>
2024-01-17 22:34:06 -0600

    8 v4

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -32,7 +32,8 @@ class BasicAuth(Auth):
         except(base64.binascii.Error, UnicodeDecodeError):
             return None
 
-    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
         """returns the user email and password
         from the Base64 decoded value"""
         if decoded_base64_authorization_header is None:
@@ -41,5 +42,6 @@ class BasicAuth(Auth):
             return None, None
         if ':' not in decoded_base64_authorization_header:
             return None, None
-        user_email, user_password = decoded_base64_authorization_header.split(':', 1)
+        user_email, user_password =\
+            decoded_base64_authorization_header.split(':', 1)
         return user_email, user_password

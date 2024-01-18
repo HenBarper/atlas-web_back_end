70b775716fea65f89d3eb1a464e4bf45ce47af9e
henbarper <benjaminitonio@gmail.com>
2024-01-17 22:04:06 -0600

    8 v1

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -17,3 +17,16 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """returns the decoded value of a Base64
+        string base64_authorization_header"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            base64_authorization_header = base64.b64decode(base64_authorization_header)
+            return base64.b64encode(base64_authorization_header).decode('utf-8') == base64_authorization_header
+        except(base64.binascii.Error, UnicodeDecodeError):
+            return None

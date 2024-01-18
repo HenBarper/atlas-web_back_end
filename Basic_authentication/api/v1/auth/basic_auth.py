beba5cc89772936d6c7a5dcb319fb2e00862e4e1
henbarper <benjaminitonio@gmail.com>
2024-01-17 21:43:04 -0600

    7 v1

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 """We will update this later"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """Empty Basic auth class"""
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """"""
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header[6:]

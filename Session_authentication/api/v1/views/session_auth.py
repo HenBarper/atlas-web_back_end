1b692c52ad95af97698a8bca3ee9e4b26cf9a526
henbarper <benjaminitonio@gmail.com>
2024-01-23 14:01:55 -0600

    test

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -8,21 +8,30 @@ from models.user import User
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login():
-    """Method to login"""
-    pass
-    # email = request.form.get(request.email)
-    # if not email:
-    #     return jsonify({"error": "email missing"}, 400)
-    # pwd = request.form.get(request.password)
-    # if not pwd:
-    #     return jsonify({"error": "password missing"}, 400)
-    # user = User()
-    # user.search(email)
-    # if not user:
-    #     return jsonify({"error": "no user found for this email"}, 404)
-    # if not user.is_valid_password(pwd):
-    #     return jsonify({"error": "wrong password"}, 401)
-    # else:
-    #     from api.v1.app import auth
-    #     session_id = auth.create_session()
-    #     return (User(session_id)).to_json()
+    """Route for session authentication"""
+    email = request.form.get('email')
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+
+    password = request.form.get('password')
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+
+    users = User.search({"email": email})
+    if not users:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    user = users[0]
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+
+    user_dict = user.to_json()
+
+    session_id = auth.create_session(user.id)
+    session_cookie_name = os.getenv('SESSION_NAME')
+    response = jsonify(user_dict)
+    response = make_response(response)
+
+    response.set_cookie(session_cookie_name, session_id)
+
+    return response

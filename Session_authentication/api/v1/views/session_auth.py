f79c193120679517ef92a0be5a085f231d8e4c2a
henbarper <benjaminitonio@gmail.com>
2024-01-22 21:52:49 -0600

    7 v1 incomplete

---
+++ Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Create a new Flask view that handles
+all routes for the Session authentication"""
+from api.v1.views import app_views
+from flask import request, jsonify
+from models.user import User
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login_method():
+    """Method to login"""
+    email = request.form.get(request.email)
+    if not email:
+        return jsonify({ "error": "email missing" }, 400)
+    pwd = request.form.get(request.password)
+    if not pwd:
+        return jsonify({ "error": "password missing" }, 400)
+    user = User.search(email)
+    if not user:
+        return jsonify({ "error": "no user found for this email" }, 404)
+    if not user.is_valid_password(pwd):
+        return jsonify({ "error": "wrong password" }, 401)
+    else:
+        pass
+    

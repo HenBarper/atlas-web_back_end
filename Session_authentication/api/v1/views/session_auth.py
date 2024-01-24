15b8e457d4420eb668244ed877ec3b35f9f481df
henbarper <benjaminitonio@gmail.com>
2024-01-24 12:37:33 -0600

    8 v2

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -32,11 +32,13 @@ def login():
 
     return response
 
-@app_views.route('/api/v1/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route('/api/v1/auth_session/logout',
+                 methods=['DELETE'], strict_slashes=False)
 def logout():
     """Method to logout"""
     from api.v1.app import auth
-    if auth.destroy_session(request) == False:
+    if auth.destroy_session(request) is False:
         abort(404)
     else:
         return jsonify({}), 200

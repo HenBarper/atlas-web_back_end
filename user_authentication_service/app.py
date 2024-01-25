649c3b6c54a770bba5719d159b13225e4a8a3812
henbarper <benjaminitonio@gmail.com>
2024-01-25 11:00:03 -0600

    6 v3

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 """Application file"""
 from flask import Flask, request, jsonify
+# from auth import Auth
+
 app = Flask(__name__)
+# AUTH = Auth()
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET'], strict_slashes=False)
 def get_jsnofiy():
     return jsonify(request.data)
 
+
+# @app.route('/users', methods=['POST'], strict_slashes=False)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")

31bc753a39a0c495c916662ba6c17a61b11e842a
henbarper <benjaminitonio@gmail.com>
2024-01-24 22:56:09 -0600

    5 v1

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python3
 """Authorization file!"""
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.exc import ValueError
 
 
-def _hash_password(password: str) -> bytes:
-    """takes in a password string arguments and returns bytes"""
-    hash_pass = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-    return hash_pass
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """take mandatory email and password
+        string arguments and return a User"""
+        if self._db.find_user_by(email=email):
+            raise ValueError(f'User {email} already exits')
+        hash = self._hash_password(password)
+        new_user = self._db.add_user(email, hash)
+        return new_user
+
+    def _hash_password(self, password: str) -> bytes:
+        """takes in a password string arguments and returns bytes"""
+        hash_pass = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        return hash_pass

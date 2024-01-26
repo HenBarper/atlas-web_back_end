5888fa67c5d1fa189f3a7560e62503d3e6824911
henbarper <benjaminitonio@gmail.com>
2024-01-26 16:23:22 -0600

    0 v2

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -7,22 +7,12 @@ from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
     """Utility testing class"""
-    # Example of syntax
-    # def test_upper(self) -> str:
-    #     """Test upper"""
-    #     self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_1(self):
-        """first sample test case: test_1()"""
-        print("test one running")
-        pass
-
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_param_1(self, nested_map, path, expected_output):
+    def test_access_nested_map(self, nested_map, path, expected_output):
         """testing test_access_nested_map()"""
         self.assertEqual(access_nested_map(nested_map, path), expected_output)
         pass

33887006986b7fc52e095f13bdcf57bb0aeb0e60
henbarper <benjaminitonio@gmail.com>
2024-01-27 13:55:26 -0600

    1 v5 revert

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -21,10 +21,10 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b")),
     ])
     def test_access_nested_map_exception(self, nested_map, path):
-        """Tests that a `KeyError` is raised"""
-        with self.assertRaises(KeyError) as cm:
+        """testing test_access_nested_map() for exceptions"""
+        with self.assertRaises(KeyError) as message:
             access_nested_map(nested_map, path)
-        self.assertEqual(cm.exception.args[0], path[-1])
+        self.assertEqual(str(message.exception), path[-1])
 
 
 if __name__ == '__main__':

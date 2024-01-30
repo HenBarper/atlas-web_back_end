fe4929bea063f587bc9dc9b4e2baebe5c630c0e0
henbarper <benjaminitonio@gmail.com>
2024-01-30 15:10:59 -0600

    3 v1

---
+++ i18n/3-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""3-app.y file"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """Configuration class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """function to get user locale"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """initial route to index"""
+    from flask_babel import gettext as _
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

1ca527fd29d78f32f846ba8633e3451bea997ed5
henbarper <benjaminitonio@gmail.com>
2024-01-30 13:18:41 -0600

    0 v2

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
 """0-app.y file"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """initial route to index"""
+    return render_template('/templates/index.html')

cca80211a05781c59c3401203d98d33496cb289c
henbarper <benjaminitonio@gmail.com>
2024-02-06 14:44:45 -0600

    0 v6

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- creates a table users
 -- With these attributes: id, email, name
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL,
-    email VARCHAR(255) NOT NULL AUTO_INCREMENT UNIQUE,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
 );

e89c556683ad67c165514c792c6eceaa8f30dbee
root <benjaminitonio@gmail.com>
2024-02-09 20:17:38 +0000

    6 v4 typo

--- MySQL_Advanced/6-bonus.sql
+++ MySQL_Advanced/6-bonus.sql
@@ -9,18 +9,14 @@ CREATE PROCEDURE AddBonus(
 BEGIN
     DECLARE project_id INT;
 
-    SELECT id INT project_id FROM projects WHERE name = project_name;
+    SELECT id INTO project_id FROM projects WHERE name = project_name;
 
     IF project_id IS NULL THEN
         INSERT INTO projects (name) VALUES (project_name);
         SET project_id = LAST_INSERT_ID();
     END IF;
 
-    IF EXISTS(SELECT 1 FROM corrections WHERE user_id = user_id AND project_id = project_id) THEN
-        UPDATE corrections SET score = score + score WHERE user_id = user_id AND project_id = project_id;
-    ELSE
-        INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
-    END IF;
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
 END;
 //
 

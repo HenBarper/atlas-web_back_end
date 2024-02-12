b888d43854f2f3af5fcbfe6afc1b5e7de193ee1c
henbarper <benjaminitonio@gmail.com>
2024-02-12 13:27:03 -0600

    10 v2 added delimeters

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -1,6 +1,7 @@
 -- Write a SQL script that creates a function SafeDiv that divides
 -- (and returns) the first by the second number or returns 0 if
 -- the second number is equal to 0.
+DELIMITER //
 CREATE FUNCTION SafeDiv
 RETURNS INT
 BEGIN
@@ -12,3 +13,6 @@ BEGIN
         SET result = a / b;
     END IF;
 END;
+//
+
+DELIMITER ;

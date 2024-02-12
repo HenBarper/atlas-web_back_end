58e33dc21f78120f3e17581314436a9289b9a5c1
henbarper <benjaminitonio@gmail.com>
2024-02-12 13:22:39 -0600

    10 v1

---
+++ MySQL_Advanced/10-div.sql
@@ -0,0 +1,14 @@
+-- Write a SQL script that creates a function SafeDiv that divides
+-- (and returns) the first by the second number or returns 0 if
+-- the second number is equal to 0.
+CREATE FUNCTION SafeDiv
+RETURNS INT
+BEGIN
+    DECLARE result INT;
+
+    IF b = 0
+        SET result = 0;
+    ELSE
+        SET result = a / b;
+    END IF;
+END;

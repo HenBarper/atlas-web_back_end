05e35951b1afebcd98b1785266c7df9e6fa4b5a9
henbarper <benjaminitonio@gmail.com>
2024-02-13 14:43:35 -0600

    10 v2

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -11,4 +11,4 @@ approached in the school"""
 
 def update_topics(mongo_collection, name, topics):
     """updates the changes to topics"""
-    mongo_collection.update({"name": name}, {"$set": {"topics": topics}}, {"multi": "true"})
+    return mongo_collection.update({"name": name}, {"$set": {"topics": topics}}, {"multi": "true"})

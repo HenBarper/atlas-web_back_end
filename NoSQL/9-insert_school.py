aa22af8b3554785d0b20c7075c9d858c374e1337
henbarper <benjaminitonio@gmail.com>
2024-02-13 14:39:22 -0600

    9 v2

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Write a Python function that inserts a new document
-in a collection based on kwargs
+   in a collection based on kwargs
 
 Prototype: def insert_school(mongo_collection, **kwargs):
 mongo_collection will be the pymongo collection object

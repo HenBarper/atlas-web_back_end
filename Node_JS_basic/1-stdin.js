7cd478a6c81746a29338e28ebd2ab6424a2dd419
henbarper <benjaminitonio@gmail.com>
2024-02-21 16:59:51 -0600

    1 v3 test2

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,12 @@
-// Task 1 - Creat a program named 1-stdin.js that exes through command line
-
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data}`);
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });

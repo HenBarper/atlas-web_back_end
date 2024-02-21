b7b66fff901a16c1a0b291057ce2c06f8310ef22
henbarper <benjaminitonio@gmail.com>
2024-02-21 16:47:54 -0600

    Node.js setup and 0v1

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,7 @@
+// In the file 0-console.js, create a function named displayMessage that prints in STDOUT the string argument.
+
+function displayMessage(message) {
+    console.log(message);
+}
+
+module.exports = displayMessage;

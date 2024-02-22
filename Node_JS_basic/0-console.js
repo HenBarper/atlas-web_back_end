8597159ab8fb46b103842f9277bf3670083ffbac
henbarper <benjaminitonio@gmail.com>
2024-02-22 14:04:09 -0600

    0 lint

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,7 +1,5 @@
-// In the file 0-console.js, create a function named displayMessage that prints in STDOUT the string argument.
-
 function displayMessage(message) {
-    console.log(message);
+  console.log(message);
 }
 
 module.exports = displayMessage;

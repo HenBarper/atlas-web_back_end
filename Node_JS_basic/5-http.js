6a3a4b4b9d43cf1cfe5cacdfb549d26283ab838d
henbarper <benjaminitonio@gmail.com>
2024-02-22 15:41:10 -0600

    5 v1

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  }
+  if (req.url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    countStudents(process.argv[2]);
+    // console.log(process.argv[2]);
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;

71fbcf004b46bc793afc3e6e034eb9929394ba78
henbarper <benjaminitonio@gmail.com>
2024-02-21 22:54:40 -0600

    2 v3

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,28 +1,39 @@
-// Task 2 - Using the db create a function countStudents in the file 2-read_file.js
+// Using the database database.csv (provided in project description), create a function countStudents in the file 2-read_file.js
+
+// Create a function named countStudents. It should accept a path in argument
+// The script should attempt to read the database file synchronously
+// If the database is not available, it should throw an error with the text Cannot load the database
+// If the database is available, it should log the following message to the console Number of students: NUMBER_OF_STUDENTS
+// It should log the number of students in each field, and the list with the following format: Number of students in FIELD: 6. List: LIST_OF_FIRSTNAMES
+// CSV file can contain empty lines (at the end) - and they are not a valid student!
 
 const fs = require('fs');
+const path = require('path');
 
-function countStudents(path) {
-  if (!fs.existsSync(path)) {
-    throw new Error('Cannot load the database');
-  }
+function countStudents(pathArg) {
+    fs.readFile(path.join(__dirname, pathArg), 'utf8', (err, data) => {
+        if (err) {
+            console.error("Cannot load the database");
+        } else {
+            const lines = data.split('\n').filter(line => line.trim() !== '');
+            console.log(`Number of students: ${lines.slice(1).length}`);
 
-  const data = fs.readFileSync(path, 'utf8');
-  const students = data.trim().split('\n').slice(1); // Directly omit header
-  const studentsByField = {};
-
-  students.forEach((student) => {
-    const [firstName,, , field] = student.split(',');
-    if (!studentsByField[field]) studentsByField[field] = [];
-    studentsByField[field].push(firstName);
-  });
-
-  const totalStudents = students.length;
-  console.log(`Number of students: ${totalStudents}`);
-
-  Object.entries(studentsByField).forEach(([field, names]) => {
-    console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
-  });
+            let cs_students = []
+            let swe_students = []
+            for( let i = 1; i < lines.length; i++) {
+                const row = lines[i].split(',');
+                if(row[3] === 'CS') {
+                    cs_students.push(row[0]);
+                }
+                if(row[3] === 'SWE') {
+                    swe_students.push(row[0]);
+                }
+            }
+              
+            console.log(`Number of students in CS: ${cs_students.length}. List: ${cs_students.join(', ')}`);
+            console.log(`Number of students in CS: ${swe_students.length}. List: ${swe_students.join(', ')}`);
+        }
+    })
 }
 
 module.exports = countStudents;

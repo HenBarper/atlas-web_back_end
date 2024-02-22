7a8c5add7a8ef160741cace68b7681250b73d5e1
henbarper <benjaminitonio@gmail.com>
2024-02-22 14:46:13 -0600

    2 v8

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,9 +1,8 @@
 const fs = require('fs');
-// const path = require('path');
 
 function countStudents(pathArg) {
-  const data = fs.readFileSync(pathArg, 'utf8');
   try {
+    const data = fs.readFileSync(pathArg, 'utf8');
     const lines = data.split('\n').filter((line) => line.trim() !== '');
       console.log(`Number of students: ${lines.slice(1).length}`);
 
@@ -22,31 +21,8 @@ function countStudents(pathArg) {
       console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
       console.log(`Number of students in CS: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
   } catch (error) {
-    throw new Error('Cannot load the database');
+    throw Error('Cannot load the database');
   }
-  // fs.readFile(path.join(__dirname, pathArg), 'utf8', (err, data) => {
-  //   if (err) {
-  //     throw new Error('Cannot load the database');
-  //   } else {
-  //     const lines = data.split('\n').filter((line) => line.trim() !== '');
-  //     console.log(`Number of students: ${lines.slice(1).length}`);
-
-  //     const csStudents = [];
-  //     const sweStudents = [];
-  //     for (let i = 1; i < lines.length; i += 1) {
-  //       const row = lines[i].split(',');
-  //       if (row[3] === 'CS') {
-  //         csStudents.push(row[0]);
-  //       }
-  //       if (row[3] === 'SWE') {
-  //         sweStudents.push(row[0]);
-  //       }
-  //     }
-
-  //     console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
-  //     console.log(`Number of students in CS: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
-  //   }
-  // });
 }
 
 module.exports = countStudents;

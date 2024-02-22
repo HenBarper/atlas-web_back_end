40f536e607b27bc1d2f7d543f62943bbbe707b8f
henbarper <benjaminitonio@gmail.com>
2024-02-21 22:33:55 -0600

    2 test

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,53 +1,28 @@
-// Using the database database.csv (provided in project description), create a function countStudents in the file 2-read_file.js
-
-// Create a function named countStudents. It should accept a path in argument
-// The script should attempt to read the database file synchronously
-// If the database is not available, it should throw an error with the text Cannot load the database
-// If the database is available, it should log the following message to the console Number of students: NUMBER_OF_STUDENTS
-// It should log the number of students in each field, and the list with the following format: Number of students in FIELD: 6. List: LIST_OF_FIRSTNAMES
-// CSV file can contain empty lines (at the end) - and they are not a valid student!
+// Task 2 - Using the db create a function countStudents in the file 2-read_file.js
 
 const fs = require('fs');
-const path = require('path');
 
-function countStudents(pathArg) {
-    fs.readFile(path.join(__dirname, pathArg), 'utf8', (err, data) => {
-        if (err) {
-            console.error("Cannot load the database");
-        } else {
-            // console.log(" Number of students: " + data);
+function countStudents(path) {
+  if (!fs.existsSync(path)) {
+    throw new Error('Cannot load the database');
+  }
 
-            const lines = data.split('\n').filter(line => line.trim() !== '');
-            console.log(`Number of students: ${lines.slice(1).length}`);
-            // console.log(lines);
-            let csData = lines.filter((line) => {
-                let fields = line.split(',');
-                return fields[3] === 'CS';
-            });
-            let sweData = lines.filter((line) => {
-                let fields = line.split(',');
-                return fields[3] === 'SWE';
-            });
-              
-            console.log(`Number of students in CS: ${csData.length}`);
-            console.log(`Number of students in CS: ${sweData.length}`);
-            // const students = {
-            //     CS: [],
-            //     SWE: []
-            // };
+  const data = fs.readFileSync(path, 'utf8');
+  const students = data.trim().split('\n').slice(1); // Directly omit header
+  const studentsByField = {};
 
-            // for (const line of lines) {
-            //     const [firstName, , field] = line.split(',');
-            //     if (field && field.trim() in students) {
-            //         students[field.trim()].push(firstName.trim());
-            //     }
-            // }
-            // console.log(`Number of students: ${lines.length}`);
-            // for (const field in students) {
-            //     console.log(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);
-            // }
-        }
-    })
+  students.forEach((student) => {
+    const [firstName,, , field] = student.split(',');
+    if (!studentsByField[field]) studentsByField[field] = [];
+    studentsByField[field].push(firstName);
+  });
+
+  const totalStudents = students.length;
+  console.log(`Number of students: ${totalStudents}`);
+
+  Object.entries(studentsByField).forEach(([field, names]) => {
+    console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+  });
 }
 
 module.exports = countStudents;

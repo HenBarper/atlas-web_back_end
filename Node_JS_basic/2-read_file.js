7d83d1ecdc9d83c04a0d3c7b14d2984d4a3f5953
henbarper <benjaminitonio@gmail.com>
2024-02-22 14:31:37 -0600

    2 v7

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
-const path = require('path');
+// const path = require('path');
 
 function countStudents(pathArg) {
-  fs.readFile(path.join(__dirname, pathArg), 'utf8', (err, data) => {
-    if (err) {
-      throw new Error('Cannot load the database');
-    } else {
-      const lines = data.split('\n').filter((line) => line.trim() !== '');
+  const data = fs.readFileSync(pathArg, 'utf8');
+  try {
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
       console.log(`Number of students: ${lines.slice(1).length}`);
 
       const csStudents = [];
@@ -23,8 +21,32 @@ function countStudents(pathArg) {
 
       console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
       console.log(`Number of students in CS: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
-    }
-  });
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+  // fs.readFile(path.join(__dirname, pathArg), 'utf8', (err, data) => {
+  //   if (err) {
+  //     throw new Error('Cannot load the database');
+  //   } else {
+  //     const lines = data.split('\n').filter((line) => line.trim() !== '');
+  //     console.log(`Number of students: ${lines.slice(1).length}`);
+
+  //     const csStudents = [];
+  //     const sweStudents = [];
+  //     for (let i = 1; i < lines.length; i += 1) {
+  //       const row = lines[i].split(',');
+  //       if (row[3] === 'CS') {
+  //         csStudents.push(row[0]);
+  //       }
+  //       if (row[3] === 'SWE') {
+  //         sweStudents.push(row[0]);
+  //       }
+  //     }
+
+  //     console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+  //     console.log(`Number of students in CS: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+  //   }
+  // });
 }
 
 module.exports = countStudents;

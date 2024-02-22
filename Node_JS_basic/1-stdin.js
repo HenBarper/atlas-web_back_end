0758a384698693fe64897a034f1bc53dc19ea0e0
henbarper <benjaminitonio@gmail.com>
2024-02-22 14:01:16 -0600

    1 lint test

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,19 +1,12 @@
-// Create a program named 1-stdin.js that will be executed through command line:
-
-// It should display the message Welcome to Holberton School, what is your name? (followed by a new line)
-// The user should be able to input their name on a new line
-// The program should display Your name is: INPUT
-// When the user ends the program, it should display This important software is now closing (followed by a new line)
-
-console.log("Welcome to Holberton School, what is your name?")
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('readable', () => {
-    let name = process.stdin.read();
-    if (name !== null) {
-        process.stdout.write(`Your name is: ${name}`);
-      }
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
 process.stdin.on('end', () => {
-    console.log('This important software is now closing');
+  console.log('This important software is now closing');
 });

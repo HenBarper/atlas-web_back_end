87469ed2a8c0f726132292a4fcb4348edf3a19ea
henbarper <benjaminitonio@gmail.com>
2024-02-26 14:23:26 -0600

    2 v4

--- unittests_in_js/2-calcul_chai.test.js
+++ unittests_in_js/2-calcul_chai.test.js
@@ -1,24 +1,24 @@
-const calculateNumber = require('./1-calcul');
+const calculateNumber = require('./2-calcul');
 const expect = require('chai').expect;
 
 
 describe('calculateNumber', () => {
   describe('SUM', () => {
     it('should return rounded sum for SUM', function() {
-      expect.strictEqual(calculateNumber('SUM', 6.2, 11.8)).to.equal(18);
+      expect(calculateNumber('SUM', 6.2, 11.8)).to.equal(18);
     });
   });
   describe('SUBTRACT', () => {
     it('should return rounded difference for SUBTRACT', function() {
-        expect.strictEqual(calculateNumber('SUBTRACT', 15.8, 6.2)).to.equal(10);
+        expect(calculateNumber('SUBTRACT', 15.8, 6.2)).to.equal(10);
     });
   });
   describe('DIVIDE', () => {
     it('should return rounded division for DIVIDE', function() {
-        expect.strictEqual(calculateNumber('DIVIDE', 9.3, 2.7)).to.equal(3);
+        expect(calculateNumber('DIVIDE', 9.3, 2.7)).to.equal(3);
     });
     it('should return Error for DIVIDE when b is 0', function() {
-        expect.strictEqual(calculateNumber('DIVIDE', 56.9, 0)).to.equal('Error');
+        expect(calculateNumber('DIVIDE', 56.9, 0)).to.equal('Error');
     });
   });
 });

f08f953d907b347f9223153fcee6a05b6ca4bb89
henbarper <benjaminitonio@gmail.com>
2024-02-26 13:44:27 -0600

    2 v10

--- unittests_in_js/1-calcul.test.js
+++ unittests_in_js/1-calcul.test.js
@@ -2,25 +2,25 @@ const calculateNumber = require('./1-calcul');
 const assert = require('assert');
 
 describe('calculateNumber', () => {
-  it('should return rounded sum for SUM', () => {
+  it('should return rounded sum for SUM', function() {
     assert.strictEqual(calculateNumber('SUM', 6.2, 11.8), 18);
   });
-  it('should return rounded sum for SUM', () => {
+  it('should return rounded sum for SUM', function() {
     assert.strictEqual(calculateNumber('SUM', -2.1, 22.4), 20);
   });
-  it('should return rounded difference for SUBTRACT', () => {
+  it('should return rounded difference for SUBTRACT', function() {
     assert.strictEqual(calculateNumber('SUBTRACT', 15.8, 6.2), 10);
   });
-  it('should return rounded difference for SUBTRACT', () => {
+  it('should return rounded difference for SUBTRACT', function() {
     assert.strictEqual(calculateNumber('SUBTRACT', 5.1, -7.9), -3);
   });
-  it('should return rounded division for DIVIDE', () => {
+  it('should return rounded division for DIVIDE', function() {
     assert.strictEqual(calculateNumber('DIVIDE', 9.3, 2.7), 3);
   });
-  it('should return rounded division for DIVIDE', () => {
+  it('should return rounded division for DIVIDE', function() {
     assert.strictEqual(calculateNumber('DIVIDE', 5.8, 3.1), 2);
   });
-  it('should return Error for DIVIDE when b is 0', () => {
+  it('should return Error for DIVIDE when b is 0', function() {
     assert.strictEqual(calculateNumber('DIVIDE', 56.9, 0), 'Error');
   });
 });

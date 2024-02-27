4c1a694bf08c2f40192723aca805a675cb74dc35
Ben Harper <benjaminitonio@gmail.com>
2024-02-27 15:13:18 -0600

    9 v2

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -2,7 +2,18 @@ const request = require('request');
 const expect = require('chai').expect;
 
 describe('Index page', function() {
-    // no longer needed
+    it('Correct status code?', function(done) {
+        request('http://localhost:7865', function (error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+    it('Correct result?', function(done) {
+        request('http://localhost:7865', function (error, response, body) {
+            expect(body).to.equal('Welcome to the payment system');
+            done();
+        });
+    });
 });
 
 describe('Cart page', function () {

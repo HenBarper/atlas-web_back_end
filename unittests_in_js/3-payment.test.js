77ff85edbe8c189d040325e643c14de820954b5e
Ben Harper <benjaminitonio@gmail.com>
2024-02-27 12:39:38 -0600

    3v9 revert

--- unittests_in_js/3-payment.test.js
+++ unittests_in_js/3-payment.test.js
@@ -1,19 +1,17 @@
+const expect = require('chai').expect;
 const sinon = require('sinon');
-const chai = require('chai');
-const expect = chai.expect;
-
-const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
 
-describe('sendPaymentRequestToApi', () => {
+describe('sendPaymentRequestToApi', function() {
+  let sinonSpy;
   afterEach(() => {
-    sinon.restore();
+    sinonSpy.restore();
   });
 
-  it('should call Utils.calculateNumber with correct arguments', () => {
-    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+  it('two funcs should match', function() {
+    sinonSpy = sinon.spy(Utils, 'calculateNumber');
     sendPaymentRequestToApi(100, 20);
-    expect(calculateNumberSpy.calledOnce).to.be.true;
-    expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
+    expect(sinonSpy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
   });
 });

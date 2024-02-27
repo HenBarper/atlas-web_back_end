8c29b6815f038ba9dbd8b6b6967ad67464115a41
henbarper <benjaminitonio@gmail.com>
2024-02-27 12:01:33 -0600

    3 v1

--- unittests_in_js/3-payment.test.js
+++ unittests_in_js/3-payment.test.js
@@ -2,3 +2,12 @@ const expect = require('chai').expect;
 const sinon = require('sinon');
 const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', function() {
+    it('two funcs should match', function() {
+        let sinonSpy = sinon.spy(100, 20);
+        let utilsSum = Utils.calculateNumber('SUM', 100, 20);
+        sendPaymentRequestToApi(sinonSpy);
+        expect(sinonSpy).to.equal(utilsSum);
+    });
+});

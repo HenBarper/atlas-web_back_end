53d3df9170b83e107e08930e1766eaf91b6154c2
Ben Harper <benjaminitonio@gmail.com>
2024-02-27 14:54:47 -0600

    8 v3

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,24 +1,17 @@
 const request = require('request');
-const { expect } = require('chai');
-const app = require('./api');
+const expect = require('chai').expect;
 
 describe('Index page', function() {
-    it('Correct status code?', function() {
+    it('Correct status code?', function(done) {
         request('http://localhost:7865', function (error, response, body) {
             expect(response.statusCode).to.equal(200);
             done();
         });
     });
-    it('Correct result?', function() {
+    it('Correct result?', function(done) {
         request('http://localhost:7865', function (error, response, body) {
             expect(body).to.equal('Welcome to the payment system');
             done();
         });
     });
-    // it('Other?', function() {
-    //     request('http://localhost:7865', function (error, response, body) {
-    //         expect(body).to.equal('Welcome to the payment system');
-    //         done();
-    //     });
-    // });
 });
